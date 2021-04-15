ccc29949ca96dc11d6ee5b0a6d8a62f92cc2071f
nemostatus <mattcorralejo1@gmail.com>
2021-04-14 20:08:27 -0700

    copy and paste mozillas example of lexical scoping

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 // alert("it's connected")
 
-function outerFn(){
-    let outsideVarThatExistsInMemoryEvenAfterOuterFnIsPoppedOffExecutionStack = "This is one crazy variable name"
-    function innerFn(){
-alert(outsideVarThatExistsInMemoryEvenAfterOuterFnIsPoppedOffExecutionStack)
-    }
-    return innerFn
-}
+// function outerFn(){
+//     let outsideVarThatExistsInMemoryEvenAfterOuterFnIsPoppedOffExecutionStack = "This is one crazy variable name"
+//     function innerFn(){
+// alert(outsideVarThatExistsInMemoryEvenAfterOuterFnIsPoppedOffExecutionStack)
+//     }
+//     return innerFn
+// }
 
-let outer = outerFn() //in window it doesnt identify it as a closure in scope, check why
-outer()
+// let outer = outerFn() //in window it doesnt identify it as a closure in scope, check why
+// outer()
+
+//mozillas example of lexical scoping
+function init() {
+    var name = 'Mozilla'; // name is a local variable created by init
+    function displayName() { // displayName() is the inner function, a closure
+      alert(name); // use variable declared in the parent function
+    }
+    displayName();
+  } 
+  init();
